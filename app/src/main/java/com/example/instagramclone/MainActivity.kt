d1664c8c9b1ee9807181e303e1bfe364d84d75dd
Celis98 <manuel.celis@uptc.edu.co>
2024-08-21 22:36:07 -0500

    Fix errors

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var ibLike: ImageButton
-    private lateinit var ibComment: ImageButton
-    private lateinit var ibShare: ImageButton
-    private lateinit var ibSave: ImageButton
     private lateinit var llPosts: LinearLayout
     private val postList = arrayListOf<Post>()
 
@@ -19,31 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        initListeners()
         fillPostList()
         addPostsToLL()
-    }
-
-    private fun initListeners() {
-        /**
-         * showToast function can be called from our activity since AppCompactActivity
-         * extends from Context
-         *
-         * kotlin automatically understand the context of every class so it implicitly knows that
-         * showToast can be called here, it allows us to omit using this
-         */
-        ibLike.setOnClickListener {
-            showToast("ibLike")
-        }
-        ibComment.setOnClickListener {
-            this.showToast("ibComment")
-        }
-        ibShare.setOnClickListener {
-            showToast("ibShare")
-        }
-        ibSave.setOnClickListener {
-            showToast("ibSave")
-        }
     }
 
     private fun fillPostList() {
@@ -56,20 +30,39 @@ class MainActivity : AppCompatActivity() {
         postList.forEach { post ->
             /**
              * When using LinearLayout, the inflated view needs a null root
+             *
              * avoid doing this when inflating any other view
              */
             val postView = layoutInflater.inflate(R.layout.home_post, null)
+
             val tvUsername: TextView = postView.findViewById(R.id.tvUsername)
             tvUsername.text = post.userName
+
+            /**
+             * showToast function can be called from our activity since AppCompactActivity
+             * extends from Context
+             *
+             * kotlin automatically understand the context of every class so it implicitly knows that
+             * showToast can be called here, it allows us to omit using this
+             */
+
+            val ibLike = postView.findViewById<ImageButton>(R.id.ibLike)
+            ibLike.setOnClickListener {
+                showToast("ibLike ${post.userName}")
+            }
+
+            /**
+             * Kotlin offers different ways to access objects and functions
+             */
+            postView.findViewById<ImageButton>(R.id.ibComment).setOnClickListener {
+                showToast("ibComment ${post.userName}", Toast.LENGTH_LONG)
+            }
+
             llPosts.addView(postView)
         }
     }
 
     private fun initViews() {
-        ibLike = findViewById(R.id.ibLike)
-        ibComment = findViewById(R.id.ibComment)
-        ibShare = findViewById(R.id.ibShare)
-        ibSave = findViewById(R.id.ibSave)
         llPosts = findViewById(R.id.ll_posts)
     }
 }
